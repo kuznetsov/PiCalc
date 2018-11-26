9629bde5d5f511ecef3fc0c4b1228f296e4b3cfe
rkuznetsov1 <rpkuznetsov@gmail.com>
2018-11-26 15:54:48 +0300

    Add multithreaded test

--- chudnovsky/src/main/java/com/apps/elliotgrin/chudnovsky/Main.java
+++ chudnovsky/src/main/java/com/apps/elliotgrin/chudnovsky/Main.java
@@ -9,9 +9,22 @@ public class Main {
         System.out.println("Enter k:");
         Scanner scanner = new Scanner(System.in);
         Long k = scanner.nextLong();
+
+        // Single threaded test
+
         Date start = new Date();
         String pi = ChudnovskyAlgorithm.calculatePi(k).toString(true);
         Double time = (new Date().getTime() - start.getTime()) / 1000.0;
+        System.out.println("Single threaded test:");
+        System.out.println("Pi = " + pi + ";\nTime = " + time.toString() + " s.");
+        System.out.println();
+
+        // Multithreaded test
+
+        start = new Date();
+        pi = ChudnovskyAlgorithm.calculatePi(k, 4).toString(true);
+        time = (new Date().getTime() - start.getTime()) / 1000.0;
+        System.out.println("Multithreaded test");
         System.out.println("Pi = " + pi + ";\nTime = " + time.toString() + " s.");
     }
 
